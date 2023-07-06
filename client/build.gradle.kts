3e5ab7bff8fbd56000db5fff17e14b45f75a4edb
S-furi <stefano.furi@studio.unibo.it>
2023-07-06 17:41:20 +0200

    Formatted graphql dependencies

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
 
     implementation("com.expediagroup", "graphql-kotlin-client", graphQLVersion)
     implementation("com.expediagroup", "graphql-kotlin-ktor-client", "7.0.0-alpha.6")
-    implementation("com.expediagroup:graphql-kotlin-spring-client:$graphQLVersion")
+    implementation("com.expediagroup","graphql-kotlin-spring-client", graphQLVersion)
 }
 
 tasks.withType<Test> { useJUnitPlatform() }
