346be4f56fda3caad175ed51d98c6146c23667a3
S-furi <stefano.furi@studio.unibo.it>
2023-07-07 11:07:53 +0200

    Created default (simple) GraphQL-Ktor Client

---
+++ client/src/main/kotlin/client/DefaultGraphQLClient.kt
@@ -0,0 +1,45 @@
+import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
+import com.expediagroup.graphql.client.types.GraphQLClientRequest
+import generated.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import java.net.URL
+import java.util.concurrent.TimeUnit
+import kotlinx.coroutines.runBlocking
+
+
+class DefaultGraphQLClient(val host: String = "localhost", val port: Int = 8080) {
+    private val ktorClient: GraphQLKtorClient
+    val logger = Logger.DEFAULT
+
+    init {
+        ktorClient = GraphQLKtorClient(url = getUrl(), httpClient = getDefaultHttpClient())
+    }
+
+    fun getUrl(): URL = URL("http://$host:$port/graphql")
+
+    fun getDefaultHttpClient() =
+            HttpClient(engineFactory = OkHttp) {
+                engine {
+                    config {
+                        connectTimeout(10, TimeUnit.SECONDS)
+                        readTimeout(60, TimeUnit.SECONDS)
+                        writeTimeout(60, TimeUnit.SECONDS)
+                    }
+                }
+                install(Logging) {
+                    logger = logger
+                    level = LogLevel.HEADERS
+                }
+            }
+
+    suspend fun <T : Any> execute(request: GraphQLClientRequest<T>) = runBlocking {
+        ktorClient.execute(request)
+    }
+
+    fun close() = ktorClient.close()
+}
--- client/src/main/kotlin/client/GraphQLClientApplication.kt
+++ client/src/main/kotlin/client/GraphQLClientApplication.kt
@@ -1,30 +1,13 @@
 package client
 
-import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
-import com.expediagroup.graphql.client.serialization.GraphQLClientKotlinxSerializer
+import DefaultGraphQLClient
 import com.expediagroup.graphql.client.types.GraphQLClientResponse
 import generated.*
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.plugins.logging.DEFAULT
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
-import java.net.URL
-import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.runBlocking
 
-val logger = Logger.DEFAULT
-
 fun main() {
-    val httpClient = getDefaultHttpClient()
-
-    val client =
-            GraphQLKtorClient(
-                    url = URL("http://localhost:8080/graphql"),
-                    httpClient = httpClient,
-                    serializer = GraphQLClientKotlinxSerializer()
-            )
+    val client = DefaultGraphQLClient()
+    val logger = client.logger
 
     logger.log("Simple book retrieval example")
 
@@ -36,23 +19,8 @@ fun main() {
     client.close()
 }
 
-fun getDefaultHttpClient() =
-        HttpClient(engineFactory = OkHttp) {
-            engine {
-                config {
-                    connectTimeout(10, TimeUnit.SECONDS)
-                    readTimeout(60, TimeUnit.SECONDS)
-                    writeTimeout(60, TimeUnit.SECONDS)
-                }
-            }
-            install(Logging) {
-                logger = logger
-                level = LogLevel.HEADERS
-            }
-        }
-
 fun executeBookQuery(
-        client: GraphQLKtorClient,
+        client: DefaultGraphQLClient,
         vars: SimpleBookQuery.Variables
 ): GraphQLClientResponse<SimpleBookQuery.Result> = runBlocking {
     client.execute(SimpleBookQuery(variables = vars))
