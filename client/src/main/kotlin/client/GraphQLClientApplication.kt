952069f38e31882e6a4a1bb1466b2b9e9978bfb4
S-furi <stefano.furi@studio.unibo.it>
2023-07-07 10:48:39 +0200

    Extracted function

--- client/src/main/kotlin/client/GraphQLClientApplication.kt
+++ client/src/main/kotlin/client/GraphQLClientApplication.kt
@@ -2,6 +2,8 @@ package client
 
 import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
 import com.expediagroup.graphql.client.serialization.GraphQLClientKotlinxSerializer
+import com.expediagroup.graphql.client.types.GraphQLClientResponse
+import generated.*
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.logging.DEFAULT
@@ -12,41 +14,46 @@ import java.net.URL
 import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.runBlocking
 
-import generated.SimpleBookQuery
-
 val logger = Logger.DEFAULT
 
 fun main() {
-    val httpClient =
-            HttpClient(engineFactory = OkHttp) {
-                engine {
-                    config {
-                        connectTimeout(10, TimeUnit.SECONDS)
-                        readTimeout(60, TimeUnit.SECONDS)
-                        writeTimeout(60, TimeUnit.SECONDS)
-                    }
-                }
-                install(Logging) {
-                    logger = logger
-                    level = LogLevel.HEADERS
-                }
-            }
+    val httpClient = getDefaultHttpClient()
 
-    val client = GraphQLKtorClient(
-            url = URL("http://localhost:8080/graphql"),
-            httpClient = httpClient,
-            serializer = GraphQLClientKotlinxSerializer())
+    val client =
+            GraphQLKtorClient(
+                    url = URL("http://localhost:8080/graphql"),
+                    httpClient = httpClient,
+                    serializer = GraphQLClientKotlinxSerializer()
+            )
 
     logger.log("Simple book retrieval example")
-    runBlocking {
-        val bookQuery = SimpleBookQuery(variables = SimpleBookQuery.Variables(listOf(1, 2, 3)))
-        logger.log("Got query:\n ${bookQuery.query}\n, With variables: ${bookQuery.variables}\n")
 
-        val queryResult = client.execute(bookQuery)
-        queryResult.data?.searchBooks?.let { books ->
-            books.filterNotNull().map { it.title }.forEach(logger::log)
-        }
+    val queryResult = executeBookQuery(client, SimpleBookQuery.Variables(listOf(1, 2, 3)))
+    queryResult.data?.searchBooks?.let { books ->
+        books.filterNotNull().map { it.title }.forEach(logger::log)
     }
 
     client.close()
+}
+
+fun getDefaultHttpClient() =
+        HttpClient(engineFactory = OkHttp) {
+            engine {
+                config {
+                    connectTimeout(10, TimeUnit.SECONDS)
+                    readTimeout(60, TimeUnit.SECONDS)
+                    writeTimeout(60, TimeUnit.SECONDS)
+                }
+            }
+            install(Logging) {
+                logger = logger
+                level = LogLevel.HEADERS
+            }
+        }
+
+fun executeBookQuery(
+        client: GraphQLKtorClient,
+        vars: SimpleBookQuery.Variables
+): GraphQLClientResponse<SimpleBookQuery.Result> = runBlocking {
+    client.execute(SimpleBookQuery(variables = vars))
 }
