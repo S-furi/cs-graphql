1d7d856fa655c20ad8e3704f447570eda49cd1fa
S-furi <stefano.furi@studio.unibo.it>
2023-07-07 11:59:45 +0200

    Added Courses querie and renamed Book one

--- client/src/main/kotlin/client/GraphQLClientApplication.kt
+++ client/src/main/kotlin/client/GraphQLClientApplication.kt
@@ -2,18 +2,25 @@ package client
 
 import DefaultGraphQLClient
 import com.expediagroup.graphql.client.types.GraphQLClientResponse
+import com.expediagroup.graphql.client.types.GraphQLClientRequest
 import generated.*
 import kotlinx.coroutines.runBlocking
+import client.executeCourseQuery
 
 fun main() {
     val client = DefaultGraphQLClient()
     val logger = client.logger
 
-    logger.log("Simple book retrieval example")
-
-    val queryResult = executeBookQuery(client, SimpleBookQuery.Variables(listOf(1, 2, 3)))
-    queryResult.data?.searchBooks?.let { books ->
+    logger.log("*****Simple book retrieval example*****")
+    val bookQueryResult = executeBookQuery(client, BookQuery.Variables(listOf(1, 2, 3)))
+    bookQueryResult.data?.searchBooks?.let { books ->
         books.filterNotNull().map { it.title }.forEach(logger::log)
+    }
+
+    logger.log("*****Simple Courses retrieval example*****")
+    val courseQuery = executeCourseQuery(client, CourseQuery.Variables(listOf(1, 2, 3)))
+    courseQuery.data?.searchCourses?.let { courses ->
+        courses.filterNotNull() .forEach(::println)
     }
 
     client.close()
@@ -21,7 +28,14 @@ fun main() {
 
 fun executeBookQuery(
         client: DefaultGraphQLClient,
-        vars: SimpleBookQuery.Variables
-): GraphQLClientResponse<SimpleBookQuery.Result> = runBlocking {
-    client.execute(SimpleBookQuery(variables = vars))
+        vars: BookQuery.Variables
+): GraphQLClientResponse<BookQuery.Result> = runBlocking {
+    client.execute(BookQuery(variables = vars))
+}
+
+fun executeCourseQuery(
+    client: DefaultGraphQLClient,
+    vars: CourseQuery.Variables
+): GraphQLClientResponse<CourseQuery.Result> = runBlocking {
+    client.execute(CourseQuery(variables = vars))
 }
