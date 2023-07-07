ecea837f55e2bcca39a7b95f15b8191824fc1451
S-furi <stefano.furi@studio.unibo.it>
2023-07-07 12:12:14 +0200

    Refactored `queryExecution` to a more generic version.
    Note: this version could potentially have issues due to runtime type
    checking because of the "when" clause. Keep in mind when extending or
    measuring performances.

--- client/src/main/kotlin/client/GraphQLClientApplication.kt
+++ client/src/main/kotlin/client/GraphQLClientApplication.kt
@@ -5,37 +5,39 @@ import com.expediagroup.graphql.client.types.GraphQLClientResponse
 import com.expediagroup.graphql.client.types.GraphQLClientRequest
 import generated.*
 import kotlinx.coroutines.runBlocking
-import client.executeCourseQuery
 
 fun main() {
     val client = DefaultGraphQLClient()
-    val logger = client.logger
 
-    logger.log("*****Simple book retrieval example*****")
-    val bookQueryResult = executeBookQuery(client, BookQuery.Variables(listOf(1, 2, 3)))
-    bookQueryResult.data?.searchBooks?.let { books ->
-        books.filterNotNull().map { it.title }.forEach(logger::log)
-    }
+    println("*****Simple book retrieval example*****")
+    val bookQueryResult = executeQuery(client, BookQuery(BookQuery.Variables(listOf(1, 2, 3))))
+    printQueryResult(bookQueryResult)
 
-    logger.log("*****Simple Courses retrieval example*****")
-    val courseQuery = executeCourseQuery(client, CourseQuery.Variables(listOf(1, 2, 3)))
-    courseQuery.data?.searchCourses?.let { courses ->
-        courses.filterNotNull() .forEach(::println)
-    }
+    println("*****Simple Courses retrieval example*****")
+    val courseQueryResult = executeQuery(client, CourseQuery(CourseQuery.Variables(listOf(1, 2, 3))))
+    printQueryResult(courseQueryResult)
 
     client.close()
 }
 
-fun executeBookQuery(
-        client: DefaultGraphQLClient,
-        vars: BookQuery.Variables
-): GraphQLClientResponse<BookQuery.Result> = runBlocking {
-    client.execute(BookQuery(variables = vars))
+fun printQueryResult(queryResult: GraphQLClientResponse<*>): Unit {
+    val data = queryResult.data
+    when (data) {
+        is BookQuery.Result? -> data?.searchBooks?.let { books ->
+            books.filterNotNull().map { it.title }.forEach(::println)
+        }
+        
+        is CourseQuery.Result? -> data?.searchCourses?.let { courses ->
+            courses.filterNotNull().forEach(::println)
+        }
+    
+        else -> println("Unkown result\n: $data")
+    }
 }
 
-fun executeCourseQuery(
+fun <T:Any> executeQuery(
     client: DefaultGraphQLClient,
-    vars: CourseQuery.Variables
-): GraphQLClientResponse<CourseQuery.Result> = runBlocking {
-    client.execute(CourseQuery(variables = vars))
+    query: GraphQLClientRequest<T>,
+): GraphQLClientResponse<T> = runBlocking {
+    client.execute(query)
 }
