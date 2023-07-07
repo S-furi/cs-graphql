ae4659e08e71d6c61771fc0aed1215e5a7cbe26f
S-furi <stefano.furi@studio.unibo.it>
2023-07-07 12:14:56 +0200

    Formatted code

--- client/src/main/kotlin/client/GraphQLClientApplication.kt
+++ client/src/main/kotlin/client/GraphQLClientApplication.kt
@@ -1,8 +1,8 @@
 package client
 
 import DefaultGraphQLClient
-import com.expediagroup.graphql.client.types.GraphQLClientResponse
 import com.expediagroup.graphql.client.types.GraphQLClientRequest
+import com.expediagroup.graphql.client.types.GraphQLClientResponse
 import generated.*
 import kotlinx.coroutines.runBlocking
 
@@ -10,11 +10,13 @@ fun main() {
     val client = DefaultGraphQLClient()
 
     println("*****Simple book retrieval example*****")
-    val bookQueryResult = executeQuery(client, BookQuery(BookQuery.Variables(listOf(1, 2, 3))))
+    val bookQueryResult =
+            executeQuery(client, BookQuery(BookQuery.Variables(listOf(1, 2, 3))))
     printQueryResult(bookQueryResult)
 
     println("*****Simple Courses retrieval example*****")
-    val courseQueryResult = executeQuery(client, CourseQuery(CourseQuery.Variables(listOf(1, 2, 3))))
+    val courseQueryResult =
+            executeQuery(client, CourseQuery(CourseQuery.Variables(listOf(1, 2, 3))))
     printQueryResult(courseQueryResult)
 
     client.close()
@@ -23,21 +25,17 @@ fun main() {
 fun printQueryResult(queryResult: GraphQLClientResponse<*>): Unit {
     val data = queryResult.data
     when (data) {
-        is BookQuery.Result? -> data?.searchBooks?.let { books ->
-            books.filterNotNull().map { it.title }.forEach(::println)
-        }
-        
-        is CourseQuery.Result? -> data?.searchCourses?.let { courses ->
-            courses.filterNotNull().forEach(::println)
-        }
-    
+        is BookQuery.Result? ->
+                data?.searchBooks?.let { books ->
+                    books.filterNotNull().map { it.title }.forEach(::println)
+                }
+        is CourseQuery.Result? ->
+                data?.searchCourses?.let { courses -> courses.filterNotNull().forEach(::println) }
         else -> println("Unkown result\n: $data")
     }
 }
 
-fun <T:Any> executeQuery(
-    client: DefaultGraphQLClient,
-    query: GraphQLClientRequest<T>,
-): GraphQLClientResponse<T> = runBlocking {
-    client.execute(query)
-}
+fun <T : Any> executeQuery(
+        client: DefaultGraphQLClient,
+        query: GraphQLClientRequest<T>,
+): GraphQLClientResponse<T> = runBlocking { client.execute(query) }
