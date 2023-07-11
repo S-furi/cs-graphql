db85330b88754e1d03de6a1d160622c6801d090b
S-furi <stefano.furi@studio.unibo.it>
2023-07-11 16:34:15 +0200

    Added query and mutations test (subscriptions still not working)

--- apollo-client/src/main/kotlin/apollo/client/ClientApplication.kt
+++ apollo-client/src/main/kotlin/apollo/client/ClientApplication.kt
@@ -3,32 +3,70 @@ package apollo.client
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.network.ws.WebSocketNetworkTransport
 import com.apollographql.apollo3.network.http.HttpNetworkTransport
+import com.apollographql.apollo3.network.http.ApolloClientAwarenessInterceptor
 
-import apollo.client.CourseQuery
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.delay
 
 import com.apollographql.apollo3.api.Optional
 import com.apollographql.apollo3.api.Builder
+import com.apollographql.apollo3.api.ApolloResponse
 import com.apollographql.apollo3.network.ws.GraphQLWsProtocol
 import com.apollographql.apollo3.network.okHttpClient
 import okhttp3.OkHttpClient
 
 fun main() {
-    val okHttpClient = OkHttpClient.Builder().build()
-    val url = "://localhost:8080/graphql"
-
-
     val apolloClient = ApolloClient.Builder()
-        .serverUrl("http$url")
-        .okHttpClient(okHttpClient)
-        .webSocketServerUrl("wss$url")
+        .serverUrl("http://localhost:8080/graphql")
         .subscriptionNetworkTransport(
             WebSocketNetworkTransport.Builder()
-                .protocol(GraphQLWsProtocol.Factory()) // implemented by default with graphql-kotlin
-                .serverUrl("wss$url")
+                .protocol(GraphQLWsProtocol.Factory())
+                .serverUrl("ws://localhost:8080/graphql")
                 .build()
-        )
-        .build()
+        ).build()
+
+
+
+    // Simple Query test
+    runBlocking {
+        apolloClient.query(CourseQuery(listOf(1, 2, 3)))
+            .execute()
+            .dataOrThrow()
+            .searchCourses.forEach(::println)
+    }
+
+    // Simple Mutation test
+    runBlocking {
+        apolloClient.mutation(ListAddMutation("Hello"))
+            .execute()
+            .dataOrThrow()
+            .also(::println)
+    }
+
+    // Subscriptions test (not working)
+    // runBlocking {
+    //     apolloClient.subscription(CounterSubscription(Optional.present(10))).toFlow()
+    //         .collect(::printSubscriptionDetails)
+    // }
 
     apolloClient.close()
 }
+
+fun printSubscriptionDetails(data: ApolloResponse<CounterSubscription.Data>): Unit {
+    println("""
+    Errors: ${data.errors}
+
+    Data: ${data.data}
+
+    IsLast: ${data.isLast}
+
+    Exception: ${data.exception}
+
+    Operation: ${data.operation}
+
+    Exec. Content: ${data.executionContext}
+
+    RequestUuid: ${data.requestUuid}
+    """)
+}
+
