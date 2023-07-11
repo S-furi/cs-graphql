77e7516f3ecd5a8c4e39a3498760f031fc16b7af
S-furi <stefano.furi@studio.unibo.it>
2023-07-11 12:51:58 +0200

    Added apollo plugin dependencies

--- apollo-client/build.gradle.kts
+++ apollo-client/build.gradle.kts
@@ -3,14 +3,12 @@ val logbackVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.8.22"
+    id("com.apollographql.apollo3") version "4.0.0-alpha.2"
     application
 }
 
 application {
     mainClass.set("apollo.client.ClientApplicationKt")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 java {
@@ -27,6 +25,14 @@ repositories { mavenCentral() }
 dependencies { 
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+
+    implementation("com.apollographql.apollo3:apollo-runtime:4.0.0-alpha.2")
 }
 
 tasks.withType<Test> { useJUnitPlatform() }
+
+apollo {
+  service("service") {
+    packageName.set("apollo.client")
+  }
+}
