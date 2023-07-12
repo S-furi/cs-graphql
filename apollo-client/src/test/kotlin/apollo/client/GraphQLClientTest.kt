4ce4b4f0beb3671a87b85eb3830caaa53149b6c8
S-furi <stefano.furi@studio.unibo.it>
2023-07-12 16:49:20 +0200

    Formatted test file

--- apollo-client/src/test/kotlin/apollo/client/GraphQLClientTest.kt
+++ apollo-client/src/test/kotlin/apollo/client/GraphQLClientTest.kt
@@ -1,41 +1,39 @@
 package apollo.client
 
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeAll
-
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.api.Optional
-import okhttp3.OkHttpClient
-
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.flow.toList
-
-import apollo.client.GraphQLClient
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 class GraphQLClientTest {
     private val graphQLEndpoint = "http://localhost:8080/graphql"
     private val webSocketEndpoint = "ws://localhost:8080/subscriptions"
-    private val clientBuilder =
-        GraphQLClient.Builder().serverUrl(graphQLEndpoint)
-    
-    private class ServerNotRunningException(
-        message: String = "Server must be up and running"
-    ): Exception(message)
+    private val clientBuilder = GraphQLClient.Builder().serverUrl(graphQLEndpoint)
+
+    private class ServerNotRunningException(message: String = "Server must be up and running") :
+            Exception(message)
 
     @Test
     fun testSimpleBookQuery() {
         val expectedTitle = "Campbell Biology"
 
         val client = getDefaultClient()
-        
+
         runBlocking {
-            val received = client.query(GetBooksQuery(listOf(1))).execute()
-                .data?.searchBooks?.get(0)?.title ?: throw ServerNotRunningException()
+            val received =
+                    client.query(GetBooksQuery(listOf(1)))
+                            .execute()
+                            .data
+                            ?.searchBooks
+                            ?.get(0)
+                            ?.title
+                            ?: throw ServerNotRunningException()
 
             assertTrue(received == expectedTitle)
         }
-        
+
         client.close()
     }
 
@@ -44,13 +42,15 @@ class GraphQLClientTest {
         val client = getDefaultClient()
 
         runBlocking {
-            var received = client.mutation(ListAddMutation("Hello")).execute()
-                .data?.addToList ?: throw ServerNotRunningException()
+            var received =
+                    client.mutation(ListAddMutation("Hello")).execute().data?.addToList
+                            ?: throw ServerNotRunningException()
 
             assertTrue(listOf("Hello").equals(received))
 
-            received = client.mutation(ListAddMutation("Word")).execute()
-                .data?.addToList ?: throw ServerNotRunningException()
+            received =
+                    client.mutation(ListAddMutation("Word")).execute().data?.addToList
+                            ?: throw ServerNotRunningException()
 
             assertEquals(listOf("Hello", "Word"), received, "Got $received")
         }
@@ -63,8 +63,11 @@ class GraphQLClientTest {
         val client = clientBuilder.addSubscriptionModule(webSocketEndpoint).build().client
 
         runBlocking {
-            val received  = client.subscription(CounterSubscription(Optional.present(2))).toFlow()
-                .toList().map { it.data?.counter }
+            val received =
+                    client.subscription(CounterSubscription(Optional.present(2)))
+                            .toFlow()
+                            .toList()
+                            .map { it.data?.counter }
             assertEquals(List(3) { it }, received)
         }
     }
