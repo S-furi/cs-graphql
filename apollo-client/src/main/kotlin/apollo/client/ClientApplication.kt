6246264a42eab1bb0b71c5cfe81529654806b29a
S-furi <stefano.furi@studio.unibo.it>
2023-07-12 16:48:32 +0200

    Formatted code and reduced number of subscription request

--- apollo-client/src/main/kotlin/apollo/client/ClientApplication.kt
+++ apollo-client/src/main/kotlin/apollo/client/ClientApplication.kt
@@ -1,12 +1,10 @@
 package apollo.client
 
-import com.apollographql.apollo3.api.ApolloResponse
 import com.apollographql.apollo3.api.Builder
 import com.apollographql.apollo3.api.Optional
 import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import apollo.client.getHttpClient
 
 private val webSocketURL = "ws://localhost:8080/subscriptions"
 private val graphQLEndpoint = "http://localhost:8080/graphql"
@@ -20,14 +18,17 @@ fun getHttpClient(): OkHttpClient =
 
 fun main() {
 
-    val subClient = GraphQLClient.Builder()
-        .serverUrl(graphQLEndpoint)
-        .addSubscriptionModule(webSocketURL, getHttpClient())
-        .build().client
+    val subClient =
+            GraphQLClient.Builder()
+                    .serverUrl(graphQLEndpoint)
+                    .addSubscriptionModule(webSocketURL, getHttpClient())
+                    .build()
+                    .client
 
     runBlocking {
-        subClient.subscription(CounterSubscription(Optional.present(10))).toFlow()
-            .collect { println(it.data?.counter) }
+        subClient.subscription(CounterSubscription(Optional.present(5))).toFlow().collect {
+            println(it.data?.counter)
+        }
     }
 
     subClient.close()
