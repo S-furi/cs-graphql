bf46ce8927897bff569ad335123a773dd5503578
S-furi <stefano.furi@studio.unibo.it>
2023-07-12 12:52:12 +0200

    Added simple test

--- apollo-client/src/test/kotlin/apollo/client/GraphQLClientTest.kt
+++ apollo-client/src/test/kotlin/apollo/client/GraphQLClientTest.kt
@@ -2,10 +2,30 @@ package apollo.client
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+
+import com.apollographql.apollo3.ApolloClient
+
+import kotlinx.coroutines.runBlocking
 
 class GraphQLClientTest {
+    private val graphQLEndpoint = "http://localhost:8080/graphql"
+    private val apolloClientBuilder =
+        ApolloClient.Builder().serverUrl(graphQLEndpoint)
+    
     @Test
-    fun okTest() {
-        assertTrue(true)
+    fun testSimpleBookQuery() {
+        val expectedTitle = "Campbell Biology"
+
+        val client = getDefaultClient()
+        
+        runBlocking {
+            val received = client.query(GetBooksQuery(listOf(1))).execute()
+            .data?.searchBooks?.get(0)?.title ?: throw IllegalStateException()
+
+            assertTrue(received == expectedTitle)
+        }
     }
+
+    private fun getDefaultClient(): ApolloClient = apolloClientBuilder.build()
 }
