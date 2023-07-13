382bfe4ba06bf8ef1791693d6ab4fff7b73b044f
S-furi <stefano.furi@studio.unibo.it>
2023-07-13 14:39:48 +0200

    Moved `runBlocking` to top level

--- apollo-client/src/main/kotlin/apollo/client/ClientApplication.kt
+++ apollo-client/src/main/kotlin/apollo/client/ClientApplication.kt
@@ -3,14 +3,11 @@ package apollo.client
 import com.apollographql.apollo3.api.Builder
 import com.apollographql.apollo3.api.Optional
 import kotlinx.coroutines.runBlocking
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 
 private val webSocketURL = "ws://localhost:8080/subscriptions"
 private val graphQLEndpoint = "http://localhost:8080/graphql"
 
-fun main() {
-
+fun main() = runBlocking {
     val subClient =
             GraphQLClient.Builder()
                     .serverUrl(graphQLEndpoint)
@@ -19,10 +16,8 @@ fun main() {
                     .build()
                     .client
 
-    runBlocking {
-        subClient.subscription(CounterSubscription(Optional.present(5))).toFlow().collect {
-            println(it.data?.counter)
-        }
+    subClient.subscription(CounterSubscription(Optional.present(5))).toFlow().collect {
+        println("Received: ${it.data?.counter} from server")
     }
 
     subClient.close()
