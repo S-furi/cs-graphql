f08882aaa60b37248e8181e7e0212dda62af6584
S-furi <stefano.furi@studio.unibo.it>
2023-07-17 15:10:45 +0200

    Made URLs `const`

--- apollo-client/src/main/kotlin/apollo/client/ClientApplication.kt
+++ apollo-client/src/main/kotlin/apollo/client/ClientApplication.kt
@@ -1,12 +1,9 @@
 package apollo.client
 
-import com.apollographql.apollo3.api.Builder
-import com.apollographql.apollo3.api.Optional
-import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 
-private val webSocketURL = "ws://localhost:8080/subscriptions"
-private val graphQLEndpoint = "http://localhost:8080/graphql"
+private const val webSocketURL = "ws://localhost:8080/subscriptions"
+private const val graphQLEndpoint = "http://localhost:8080/graphql"
 
 fun main() = runBlocking {
     val subClient =
